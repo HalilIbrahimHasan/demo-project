b7781e765fe96397acf0818968a067d23d01ccac
Ibrahim Kalin <hisetkal@gmail.com>
2021-06-23 19:20:13 -0400

    chrome options added

--- src/test/java/BasicTests.java
+++ src/test/java/BasicTests.java
@@ -2,6 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BasicTests {
 
@@ -11,7 +12,10 @@ public class BasicTests {
     public void test1(){
         WebDriverManager.chromedriver().setup();
         System.out.println("Driver is being set-up");
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("disable-gpu");
+        driver = new ChromeDriver(options);
         driver.get("https://www.google.com");
         System.out.println(driver.getTitle());
 
